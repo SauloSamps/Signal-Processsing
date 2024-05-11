baf40f88326377877c0a92aee6c631ce01e421ab
Saulo Sampaio <82217565+SauloSamps@users.noreply.github.com>
2024-05-10 22:21:38 -0300

    Edited Dataset_Generator.py
    Changed Dataset generator to generate n random lorentzian curves for ml training.

--- Dataset Generator.py
+++ Dataset Generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import uniform
 import matplotlib.pyplot as plt
 
 def lorentzian(x, x0, gamma, maximum):
@@ -8,6 +9,7 @@ def lorentzian(x, x0, gamma, maximum):
     
     #maximum = maximum * np.pi
     #return (maximum/np.pi) * (gamma / ((x - x0)**2 + gamma**2))
+    maximum = maximum*gamma
     return maximum * (gamma / ((x - x0)**2 + gamma**2))
 
 def generateData(gamma, x0, xmin, xmax, numberOfPoints, amplitude):
@@ -33,10 +35,10 @@ def addWhiteNoiseRandom(xData, yData, factor):
     y_noisy = yData + np.random.normal(0, factor, len(xData))
     return y_noisy
 
-def saveData(xData, yData):
+def saveData(xData, yData, name):
     # Save (x, y) data to a CSV file
     data = np.column_stack((xData, yData))
-    np.savetxt('lorentzian_data.csv', data, delimiter=',', fmt='%.5f,%.5f', header='x,y', comments='')
+    np.savetxt(name, data, delimiter=',', fmt='%.5f,%.5f', header='x,y', comments='')
     
 def showData(xData, yData):
     plt.scatter(xData, yData, label='Data Points', color='r', s=5)
@@ -47,16 +49,24 @@ def showData(xData, yData):
     plt.show()
 
 def main():
+    datasetSize = 1000
     
-    x,y = generateData(1, 0, -10, 10, 100, 10) #generates the lorentzian
-    y_noisy = addWhiteNoiseRandom(x, y, 0.1) #adds random white noise by factor
-    
-    # Round the data to at most 5 decimal places
-    x_rounded = np.around(x, decimals=5)
-    y_rounded = np.around(y_noisy, decimals=5)
-    
-    saveData(x, y_noisy)
-    showData(x_rounded, y_rounded)
+    for i in range (datasetSize):
+        gamma = np.around(uniform(1.0, 3.0), decimals=3)
+        center = np.around(uniform(-9.0, 9.0), decimals=3)
+        amplitude = np.around(uniform(4.0, 10.0), decimals=3)
+        
+        saveName = "Dataset/" + str(gamma) + "_" + str(center) + "_" + str(amplitude) + ".csv"
+        
+        x,y = generateData(gamma, center, -10, 10, 100, amplitude) #generates the lorentzian
+        y_noisy = addWhiteNoiseRandom(x, y, 0.2) #adds random white noise by factor
+        
+        # Round the data to at most 5 decimal places
+        x_rounded = np.around(x, decimals=5)
+        y_rounded = np.around(y_noisy, decimals=5)
+        
+        saveData(x, y_noisy, saveName)
+        #showData(x_rounded, y_rounded)
 
 
 if __name__ == "__main__":
